bb571de321e572a1c4dcc66e3463a5a782cc75ff
leowzhenkang <leowzk@hotmail.com>
2020-02-04 14:10:12 +0800

    changed some port stuff

--- index.js
+++ index.js
@@ -329,7 +329,7 @@ app.get('/navbar', (request, response) => {
 //Listen to requests on port 3000
 const PORT = process.env.PORT || 3000;
 
-const server = app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
+const server = app.listen(PORT, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
 
 let onClose = function() {
 
